875dd521b1fe484c17ebb5c03d216302f7317d14
Waleed-Ali-sarwar <waleed.alisarwar336@outlook.com>
2025-07-31 16:32:26 +0500

    footer changes

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
         <footer>
             Chat Bot &copy; {new Date().getFullYear()}
             <p>Developed by <a href="https://linkedin.com/in/waleed-ali-sarwar/" target="_blank"> Waleed Ali Sarwar</a></p>
-            <p>built with ❤️ using React hosted on Azure Static Web App</p>
+            <p>built with ❤️ using React hosted on Github pages</p>
         </footer>
     );
 }
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/chatbot-react-app/', // Essential for correct asset paths
+  base: '/chatbot-react-app/',
   plugins: [react()],
 })
